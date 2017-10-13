93c9d4ea63dd41f9ea26efd49ce9cfdee4c06969
Renato Carmo <renato.brgo@gmail.com>
2017-10-12 21:11:18 -0300

    Classe Conta Implementada

---
+++ TrabalhoFinal/CaixaEletronico/src/br/ufg/inf/especializacao/trabalhofinal/caixaeletronico/data/ContaDAO.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.especializacao.trabalhofinal.caixaeletronico.data;
+
+import br.ufg.inf.especializacao.trabalhofinal.caixaeletronico.model.Conta;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Renato
+ */
+public class ContaDAO {
+    private final String TABLE_NAME = "CONTA";
+    private final String COLUMN_CONTA = "CONTA";
+    private final String COLUMN_AGENCIA = "AGENCIA";
+    private final String COLUMN_TIPO = "TIPO";
+    private final String COLUMN_CPFCNPJ = "CPFCNPJ";
+    private final String COLUMN_SALDO = "SALDO";
+    private final String COLUMN_SENHA = "SENHA";
+    
+    public void create(Conta conta) throws SQLException {
+        
+        final String SQL = "INSERT INTO " + TABLE_NAME + "(" 
+                + COLUMN_CONTA + "," 
+                + COLUMN_AGENCIA + "," 
+                + COLUMN_TIPO + "," 
+                + COLUMN_CPFCNPJ + "," 
+                + COLUMN_SALDO + "," 
+                + COLUMN_SENHA 
+                + ") VALUES (" 
+                + conta.getConta() + "," 
+                + conta.getAgencia() + "," 
+                + conta.getTipo() + ","
+                + conta.getCpfcnpj() + ","
+                + conta.getSaldo() + ","
+                + conta.getSenha() + ")";
+        Connection connection = ConnectionUtils.getConnection();
+        
+        Statement cmd = null;
+        ResultSet res = null;
+        
+        try {
+            cmd = connection.createStatement();
+            cmd.executeUpdate(SQL);
+            connection.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            if(cmd !=null) {
+                cmd.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+    
+    public Conta getByCod(long nconta) throws SQLException {
+        final String QUERY = "SELECT * FROM " + TABLE_NAME + " WHERE "
+                + COLUMN_CONTA + "=" + nconta + ";";
+        Connection connection = ConnectionUtils.getConnection();
+        Statement cmd = null;
+        ResultSet res = null;
+        try {
+            cmd = connection.createStatement();
+            res = cmd.executeQuery(QUERY);
+
+            if (!res.next()) {
+                throw new RuntimeException("Conta inexistente.");
+            }
+            
+            Conta conta = new Conta();
+            conta.setConta(res.getLong(COLUMN_CONTA));
+            conta.setAgencia(res.getLong(COLUMN_AGENCIA));
+            conta.setTipo(res.getLong(COLUMN_TIPO));
+            conta.setCpfcnpj(res.getLong(COLUMN_CPFCNPJ));
+            conta.setSaldo(res.getLong(COLUMN_SALDO));
+            conta.setSenha(res.getLong(COLUMN_SENHA));
+            return conta;
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        throw new SQLException("Conta nº " + nconta + " não encontrada!");
+    }
+    
+    public List<Conta> getAll() throws SQLException{
+        final String sql = "SELECT * FROM " + TABLE_NAME;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        List<Conta> contas = new ArrayList();
+        
+        try{
+            connection = ConnectionUtils.getConnection();
+            statement = connection.prepareStatement(sql);
+            ResultSet res = statement.executeQuery();
+            while(res.next()){
+                Conta conta = new Conta();
+                conta.setConta(res.getLong(COLUMN_CONTA));
+                conta.setAgencia(res.getLong(COLUMN_AGENCIA));
+                conta.setTipo(res.getLong(COLUMN_TIPO));
+                conta.setCpfcnpj(res.getLong(COLUMN_CPFCNPJ));
+                conta.setSaldo(res.getLong(COLUMN_SALDO));
+                conta.setSenha(res.getLong(COLUMN_SENHA));
+                contas.add(conta);
+            }
+        } catch (SQLException e){
+            throw e;
+        } finally{
+            if(statement != null){
+                statement.close();
+            }
+            if(connection != null){
+                connection.close();
+            }
+        }
+        return contas;
+        
+    }
+    
+    public boolean update(Conta conta) throws SQLException {
+        String sqlUpdate = "UPDATE " + TABLE_NAME + " SET " 
+                + COLUMN_CONTA + " = ?, "
+                + COLUMN_AGENCIA + " = ?, "
+                + COLUMN_TIPO + " = ?, "
+                + COLUMN_CPFCNPJ + " = ?, "
+                + COLUMN_SALDO + " = ?, "
+                + COLUMN_SENHA + " = ?"
+                +" WHERE " + COLUMN_CONTA + "= ?";
+        Connection connection = null;
+        PreparedStatement procedure = null;
+            connection = ConnectionUtils.getConnection();
+            try { 
+                procedure = connection.prepareStatement(sqlUpdate);
+                procedure.setLong(1, conta.getConta());               
+                procedure.setLong(2, conta.getAgencia());
+                procedure.setLong(3, conta.getTipo());
+                procedure.setLong(4, conta.getCpfcnpj());
+                procedure.setLong(4, conta.getSaldo());
+                procedure.setLong(5, conta.getSenha());
+                
+                int res = procedure.executeUpdate();
+                if(res == 1){
+                    connection.commit();
+                    return true;
+                }     
+            }catch (SQLException e) {
+                throw new SQLException(e.getMessage());
+            } finally {
+                if(procedure != null){
+                    procedure.close();
+                }
+                if(connection != null){
+                    connection.close();
+                }
+            }
+            return false;
+    }
+    
+    public void delete(Conta conta) throws SQLException {
+        String query = "DELETE FROM " + TABLE_NAME
+                + " WHERE " + COLUMN_CONTA + "=" + conta.getConta() + ";";
+        ConnectionUtils.executeVoidQuery(query);
+    }
+    
+}
--- TrabalhoFinal/CaixaEletronico/src/br/ufg/inf/especializacao/trabalhofinal/caixaeletronico/model/Conta.java
+++ TrabalhoFinal/CaixaEletronico/src/br/ufg/inf/especializacao/trabalhofinal/caixaeletronico/model/Conta.java
@@ -7,8 +7,72 @@ package br.ufg.inf.especializacao.trabalhofinal.caixaeletronico.model;
 
 /**
  *
- * @author Elenilson
+ * @author Renato
  */
 public class Conta {
-    
+    private long conta;
+    private long agencia;
+    private long tipo;
+    private long cpfcnpj;
+    private long saldo;
+    private long senha;
+
+    public long getConta() {
+        return conta;
+    }
+
+    public void setConta(long conta) {
+        this.conta = conta;
+    }
+
+    public long getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(long agencia) {
+        this.agencia = agencia;
+    }
+
+    public long getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(long tipo) {
+        if(tipo<1 && tipo>2) {
+            throw new ModelException("Tipo de conta inválido.");
+        }else{
+            this.tipo = tipo;
+        }
+    }
+
+    public long getCpfcnpj() {
+        return cpfcnpj;
+    }
+
+    public void setCpfcnpj(long cpfcnpj) {
+        if (Long.toString(cpfcnpj).length() != 11 && Long.toString(cpfcnpj).length() != 14) {
+            throw new ModelException("CPF/CNPJ inválido.");
+        } else {
+            this.cpfcnpj = cpfcnpj;
+        } 
+    }
+
+    public long getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(long saldo) {
+        this.saldo = saldo;
+    }
+
+    public long getSenha() {
+        return senha;
+    }
+
+    public void setSenha(long senha) {
+        if(Long.toString(senha).length() != 6){
+            throw new ModelException("A senha deve conter 6 digitos.");
+        }
+        this.senha = senha;
+    }
 }
